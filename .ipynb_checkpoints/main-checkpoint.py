ae614719a8f1605df2d480636c5e41e1232b2b72
Septian Jauhariansyah <43625773+alchemista27@users.noreply.github.com>
2024-07-16 12:07:26 +0700

    Adding some code for dashboard

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,26 @@
+# Import the library
+import pandas as pd
+import plotly.express as px
+import dash
+from dash import dcc, html
+
+# Load the data
+ecom_sales = pd.read_csv('data/ecom_data.csv')
+major_categories = list(ecom_sales['Major Category'].unique())
+minor_categories = list(ecom_sales['Minor Category'].unique())
+ecom_country = ecom_sales.groupby('Country')['OrderValue'].agg(['sum', 'count']).reset_index().rename(columns={'sum':'Total Sales', 'count':'Sales Volume'})
+
+# Setting up the apps
+css = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+app = dash.Dash(name='E Commerce Sales', external_stylesheets = css)
+
+# layouting the app
+app.layout = html.Div(children= 
+    html.Div(children = 
+        html.A('Sales Dashboard', href = '#', className = 'navbar-brand')
+    )
+)
+
+# Starting up the server
+if __name__ == '__main__':
+    app.run_server(debug=True)
